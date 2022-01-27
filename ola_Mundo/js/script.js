6364051f57bdc1bb7c6e708f98c51715dc382523
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2022-01-27 19:52:06 -0300

    Repositório do curso de lógica com JavaScript

---
+++ ola_Mundo/js/script.js
@@ -0,0 +1,5 @@
+let nome=prompt("Digite o seu nome:")
+alert (`Olá ${nome}, seja bem vindo a imersão de Front End`)
+
+document.getElementById("saida").innerHTML=
+`Olá ${nome}, seja bem vindo a imersão de Front End`
